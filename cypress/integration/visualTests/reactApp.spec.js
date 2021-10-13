3bdd03cae685620fffd9aa9ec4ae77701436c5db
Manpreet Singh <manpreesingh@qasource.com>
2021-10-13 15:31:35 +0530

    changed appname and test name

--- cypress/integration/visualTests/reactApp.spec.js
+++ cypress/integration/visualTests/reactApp.spec.js
@@ -1,16 +1,15 @@
 /// <reference types="cypress" />
 
 describe("Testing the react app", () => {
-
   it("Should display 2 todos by default", () => {
     cy.eyesOpen({
-      appName: "React App",
+      appName: "Todo App",
       testName: "Default todos",
     });
-    cy.visit('http://localhost:3000/');
+    cy.visit("http://localhost:3000/");
     cy.eyesCheckWindow("Home Page");
-    cy.get('div:nth-child(2) > div.panel-body > button').click();
-    cy.eyesCheckWindow("button 2 info")
+    cy.get("div:nth-child(2) > div.panel-body > button").click();
+    cy.eyesCheckWindow("button 2 info");
   });
 });
 
