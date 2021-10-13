966c0eee0fafd7a37220d477293560043f0d1026
Manpreet Singh <manpreesingh@qasource.com>
2021-10-13 15:27:34 +0530

    added check

--- cypress/integration/visualTests/reactApp.spec.js
+++ cypress/integration/visualTests/reactApp.spec.js
@@ -5,10 +5,12 @@ describe("Testing the react app", () => {
   it("Should display 2 todos by default", () => {
     cy.eyesOpen({
       appName: "React App",
-      testName: "HomePage",
+      testName: "Default todos",
     });
     cy.visit('http://localhost:3000/');
     cy.eyesCheckWindow("Home Page");
+    cy.get('div:nth-child(2) > div.panel-body > button').click();
+    cy.eyesCheckWindow("button 2 info")
   });
 });
 
