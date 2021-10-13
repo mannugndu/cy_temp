8c399e6645c2d6f6e4a4e26f6259068e3efadd1f
Manpreet Singh <manpreesingh@qasource.com>
2021-10-13 16:13:50 +0530

    added wait in react tests

--- cypress/integration/visualTests/reactApp.spec.js
+++ cypress/integration/visualTests/reactApp.spec.js
@@ -1,16 +1,17 @@
 /// <reference types="cypress" />
 
 describe("Testing the react app", () => {
-
   it("Should display 2 todos by default", () => {
     cy.eyesOpen({
       appName: "React app",
       testName: "Should display 2 todos by default",
     });
-    cy.visit('http://localhost:3000/');
+    cy.visit("http://localhost:3000/");
+    cy.wait(2000);
     cy.eyesCheckWindow("Default Page of React");
-    cy.get('div:nth-child(2) > div.panel-body > button').click();
-    cy.eyesCheckWindow("button 2 info")
+    cy.get("div:nth-child(2) > div.panel-body > button").click();
+    cy.wait(2000);
+    cy.eyesCheckWindow("button 2 info");
   });
 });
 
