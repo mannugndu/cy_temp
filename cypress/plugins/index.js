4845b448f5d352386f8d4329ce51c7bb4027d327
Manpreet Singh <mannu.gndu@gmail.com>
2022-07-14 11:16:35 +0530

    11

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,12 @@
 /// <reference types="cypress" />
 
-module.exports = (on, config) => { 
+module.exports = (on, config) => {
+  if (config.env.a11yConfig == true) {
+    config.video = false;
+    config.videoCompression=false
+    config.videoUploadOnPasses = false;
+    config.retries = { runMode: 0, openMode: 0 };
+  }
+  
   console.log(config)
 }
